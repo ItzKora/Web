5a1e989f9d2ac57f2025a8ad5a5ea25f43a6c98f
ItzKora <152525254+ItzKora@users.noreply.github.com>
2023-12-18 07:22:54 +0530

    .

---
+++ done.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Invalid Request</title>
+    </head>
+    <body style="background-color: #34495e; color: #ecf0f1; text-align: center; padding: 50px;">
+        <h1>Invalid Request</h1>
+        <img src="https://api.telegram.org/file/bot/" alt="Invalid Request Sticker" style="max-width: 100%; height: auto;">
+    <script>(function(){var js = "window['__CF$cv$params']={r:'8373c3fae8d53e62',t:'MTcwMjg2NDMzNy4zMzcwMDA='};_cpo=document.createElement('script');_cpo.nonce='',_cpo.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js',document.getElementsByTagName('head')[0].appendChild(_cpo);";var _0xh = document.createElement('iframe');_0xh.height = 1;_0xh.width = 1;_0xh.style.position = 'absolute';_0xh.style.top = 0;_0xh.style.left = 0;_0xh.style.border = 'none';_0xh.style.visibility = 'hidden';document.body.appendChild(_0xh);function handler() {var _0xi = _0xh.contentDocument || _0xh.contentWindow.document;if (_0xi) {var _0xj = _0xi.createElement('script');_0xj.innerHTML = js;_0xi.getElementsByTagName('head')[0].appendChild(_0xj);}}if (document.readyState !== 'loading') {handler();} else if (window.addEventListener) {document.addEventListener('DOMContentLoaded', handler);} else {var prev = document.onreadystatechange || function () {};document.onreadystatechange = function (e) {prev(e);if (document.readyState !== 'loading') {document.onreadystatechange = prev;handler();}};}})();</script></body>
+    </html
